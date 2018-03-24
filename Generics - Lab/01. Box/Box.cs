8a8ecf8225599300946674dd6b7fa79df1133e12
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-03-24 11:57:51 +0200

    Add files via upload

---
+++ Generics - Lab/01. Box/Box.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+public class Box<T>
+{
+
+    private List<T> data;
+
+
+    public Box()
+    {
+        data = new List<T>();
+    }
+
+
+
+    public int Count
+    {
+        get
+        {
+            return this.data.Count;
+        }
+     
+    }
+
+
+    public void Add(T element)
+    {
+        this.data.Add(element);
+        
+    }
+
+    public T Remove()
+    {
+        var last = this.data.Last();
+        this.data.RemoveAt(this.data.Count-1);
+
+        return last;
+    }
+}
+
