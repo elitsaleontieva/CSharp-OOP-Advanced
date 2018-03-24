e8b8d7311d0240329b6a06e0b9c7f10fc1b18a97
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-03-24 12:21:19 +0200

    Add files via upload

---
+++ Generics - Lab/02. Array Creator/ArrayCreator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+public class ArrayCreator
+{
+
+    public static T[] Create<T>(int length, T item)
+    {
+        return new T[length];
+       
+    }
+    
+}
+
