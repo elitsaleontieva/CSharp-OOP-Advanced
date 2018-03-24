5dffde2e57fdbe351c56c82c15f7908bc47d80a4
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-03-24 11:40:16 +0200

    Add files via upload

---
+++ Generics - Lab/01. Box/Box.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+public class Box<T> : IBox<T>
+{
+
+    private List <T> data;
+      
+    public Box()
+    {
+        this.data = new List<T>();
+    }
+
+    public T this[int index]
+    {
+          get
+          {
+            if (index >= this.Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return this.data[index];
+            }
+    }
+
+    public int Count => this.data.Count;
+
+
+
+
+    public void Add(T element)
+    {
+        this.data.Add(element);
+
+        //  this.data.ToArray();
+
+
+
+    }
+
+    public T Remove()
+    {
+        var last = this.data.Last();
+        this.data.RemoveAt(this.data.Count-1);
+
+        return last;
+    }
+}
+
---
+++ Generics - Lab/01. Box/IBox.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+public interface IBox<T>
+{
+    int Count { get; }
+
+    T this[int index] { get; }
+
+    void Add(T element);
+
+    T Remove();
+}
