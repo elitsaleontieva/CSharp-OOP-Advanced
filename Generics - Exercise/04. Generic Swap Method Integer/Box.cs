187b8eedf6b525e9f68addc0a85a05f0478b3e36
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-03-26 13:20:25 +0300

    Add files via upload

---
+++ Generics - Exercise/04. Generic Swap Method Integer/Box.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Box<T>
+{
+    public T Field { get; set; }
+
+    public Box()
+    {
+    }
+
+    public Box(T field)
+    {
+        this.Field = field;
+    }
+
+  
+    public void Swap(List<Box<int>> list, int indexA, int indexB)
+    {
+
+        Box<int> tmp = list[indexA];
+        list[indexA] = list[indexB];
+        list[indexB] = tmp;
+    }
+
+
+
+    public override string ToString()
+    {
+        return $"{this.Field.GetType().FullName.ToString()}: {this.Field}";
+    }
+}
+
