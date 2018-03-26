6b3210c558bdd15d2fda5cb0f5eb76abe2c7f618
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-03-26 14:39:43 +0300

    Add files via upload

---
+++ Generics - Exercise/05. Generic Count Method String/Box.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Box<T> 
+{
+    public T Field { get; set; }
+
+    public Box()
+    {
+    }
+
+    public Box(T field)
+    {
+        this.Field = field;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Field.GetType().FullName.ToString()}: {this.Field}";
+    }
+
+    public int Compare<T>(List<T> list, T element)
+         where T : IComparable<T>
+    {
+        int counter = 0;
+        foreach (var generic in list)
+        {
+            if (generic.CompareTo(element) > 0)
+            {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
+
