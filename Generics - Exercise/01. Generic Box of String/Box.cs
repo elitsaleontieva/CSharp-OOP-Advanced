be09c169cfe11c5ff8b07e7dc378bf2aaccbf41e
Elitsa Leontieva <ellyleontieva@abv.bg>
2018-03-26 12:12:42 +0300

    Add files via upload

---
+++ Generics - Exercise/01. Generic Box of String/Box.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Box<T>
+{
+    public T Field { get; set; }
+
+    public Box(T field)
+    {
+        this.Field = field;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Field.GetType().FullName.ToString()}: {this.Field}";
+    }
+}
+
